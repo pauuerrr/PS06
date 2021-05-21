2efb3ed2d3b8cc59e32a66ecd2706809a904ea09
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-21 20:20:03 +0200

    Dodaj pliki projektów.

---
+++ PS06/CheckBrowserMiddleware.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS06
+{
+    public class CheckBrowserMiddleware
+    {
+        private RequestDelegate _next;
+    }
+}
