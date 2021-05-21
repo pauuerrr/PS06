39512a22c51d0dc4129e9841943591dc72ce77fd
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-21 20:32:36 +0200

    Napisanie metody InvokeAsync

--- PS06/CheckBrowserMiddleware.cs
+++ PS06/CheckBrowserMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,20 @@ namespace PS06
         public CheckBrowserMiddleware(RequestDelegate next)
         {
             this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext,IBrowserDetector detector)
+        {
+            var browser = detector.Browser;
+
+            if ((browser.Name == BrowserNames.Edge) || (browser.Name == BrowserNames.EdgeChromium) || (browser.Name == BrowserNames.InternetExplorer))
+            {
+                await httpContext.Response.WriteAsync("<p>Przeglądarka nie jest obsługiwana</p>");
+            }
+            else
+            {
+                await next(httpContext);
+            }
         }
     }
 }
